4d47e12571e9c2b3d4f87456354ad19b4216f5df
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-05-25 16:06:48 +0530

    Added: header text and static table

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -3,10 +3,10 @@ class EmployeePayrollData{
     set id(id){this.id = id;}
 
     get name(){return this.name;}
-    set name(name){
-        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
-        if(nameRegex.test(name)){
-            this.name = name;
+    set name(newName){
+        let nameRegex = RegExp(/^[A-Z]{1}[a-zA-Z\\s]{2,}/);
+        if(nameRegex.test(newName)){
+            this.name = newName;
         }
         else throw 'Name is Incorrect!';
     }
@@ -44,13 +44,14 @@ class EmployeePayrollData{
 window.addEventListener('DOMContentLoaded',(event)=>{
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error')
+    // let employeePayrollData = new EmployeePayrollData();
     name.addEventListener('input',function () {
         if(name.value.length == 0){
             textError.textContent = "";
             return;
         }
         try{
-            // (new EmployeePayrollData()).name = name.value;
+            (new EmployeePayrollData()).name = name.value;
             textError.textContent = "";
 
         }catch(e){
@@ -69,9 +70,22 @@ window.addEventListener('DOMContentLoaded',(event)=>{
 const save = () =>{
     try {
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStoreage(employeePayrollData);
     } catch (error) {
         
     }
+}
+
+// Local storage
+function createAndUpdateStoreage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollData!= undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList  = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayroll = () =>{
@@ -80,11 +94,64 @@ const createEmployeePayroll = () =>{
         employeePayrollData.name = getInputValueById('#name');
 
     } catch (e) {
-        
+        setTextValue('.text-error',e);
     }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#note');
+    let date = getInputValueById('#day')+""+getInputValueById('#month')+""+getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue)=>{
+    let allItems = document.querySelector(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if(item.checked) setItems.push(item.value)
+    });
+    return setItems;
 }
 
 const getInputValueById = (id) =>{
     let value = document.querySelector(id).value;
     return value;
+}
+
+const getInputElementValue = (id) =>{
+    let value = document.getElementById(id).value;
+    return value;
+}
+
+//reset form
+const resetForm = ()=>{
+    setValue('#name','');
+    unsetSelectedValue('[name = profile]');
+    unsetSelectedValue('[name = gender]');
+    unsetSelectedValue('[name = department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2022');
+}
+
+const setValue = (id , value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const unsetSelectedValue = (propertyValue)=>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item =>{
+        item.checked = false;
+    });
+}
+
+const setTextValue = (id , value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
 }
